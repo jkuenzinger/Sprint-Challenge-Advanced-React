2d8c4847614c19c1d2aee79d395f91258cd24e62
jkuenzinger <jkuenzinger228@gmail.com>
2020-03-13 08:38:51 -0700

    a

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import PlayerList from './components/PlayerList.js'
 
 
 function App() {
   return (
     <div className="containermain">
-     
+     <PlayerList/>
     </div>
   );
 }
--- client/src/components/PlayerList.js
+++ client/src/components/PlayerList.js
@@ -2,7 +2,7 @@ import React from 'react'
 // creating a class component that is goign to render all my players from the data i recieve.
 // intializing the state with an array called Players this is where im going to stick the date i recieve form the api call.
 
-export default class PlayersList extends React.Component{
+export default class PlayerList extends React.Component{
     constructor(){
         super()
             this.state={
@@ -11,8 +11,9 @@ export default class PlayersList extends React.Component{
         }
 // below im creating a componedidmount function wehre i will be fetching the players data fom the api hosted locally on my system
 // I will then set the response i get to the players array use this.setstate and set up a catch in case there is an error
-ComponentDidMount(){
+componentDidMount(){
 fetch('http://localhost:5000/api/players')
+.then(res => res.json())
 .then(res =>{this.setState({players: res})})
 .catch(err => console.log('unable to fetch players', err))
 }
@@ -23,7 +24,7 @@ render(){
     return(
         <div>
             <h2>Players</h2>
-            {this.state.people.map((player) => <p>{player.name}</p>)}
+            {this.state.players.map((player) => <p>{player.name}</p>)}
         </div>
     )
 }
